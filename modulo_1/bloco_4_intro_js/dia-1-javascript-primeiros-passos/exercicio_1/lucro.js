4c28d5cfce0bb0e295f8c03e38f82b22cd778d3e
iuryroque <iury.roque@hotmail.com>
2021-07-28 19:50:13 -0300

    Exercicio 10-lucro finalizado

---
+++ modulo_1/bloco_4_intro_js/dia-1-javascript-primeiros-passos/exercicio_1/lucro.js
@@ -0,0 +1,12 @@
+let custo_produto = 1000;
+let valor_venda = 2000;
+let lucro = 0;
+
+if (custo_produto != 0 && valor_venda != 0){
+    valor_custo_total += custo_produto + (custo_produto /100 * 20);
+    lucro = valor_venda - valor_custo_total;
+    lucro_total = lucro * 1000;
+    console.log("A empresa terá de lucro " + lucro_total;
+}else{
+    console.log("Erro");
+}
