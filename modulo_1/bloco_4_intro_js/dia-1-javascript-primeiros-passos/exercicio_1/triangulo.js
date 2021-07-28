9317e849eafc848ed6d236c07e59c2cce81c9f62
iuryroque <iury.roque@hotmail.com>
2021-07-28 17:18:03 -0300

    Exercicio 5-triangulo finalizado

---
+++ modulo_1/bloco_4_intro_js/dia-1-javascript-primeiros-passos/exercicio_1/triangulo.js
@@ -0,0 +1,15 @@
+let a = 100;
+let b = 40;
+let c = 39;
+sum = a+b+c;
+
+if (a > 0 && b > 0 && c > 0) {
+    if (sum === 180) {
+        console.log("True");
+    }else{
+        console.log("False");
+    }
+}else{
+    console.log("Erro");
+}
+
