067b0b9b065ee15bff055f453f0d0b8efb9d4ee9
iuryroque <iury.roque@hotmail.com>
2021-07-28 16:57:50 -0300

    Exercicio 2-maior de tres finalizado

---
+++ modulo_1/bloco_4_intro_js/dia-1-javascript-primeiros-passos/exercicio_1/maior_de_tres.js
@@ -0,0 +1,14 @@
+let a,b;
+a=6;
+b=1;
+c=2;
+
+if(a>b){
+    console.log(a);
+    }else if(a>c){
+        console.log(a);
+        }else if(b>c){
+            console.log(b);
+            }else{
+                console.log(c);
+            }
