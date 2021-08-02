4a99ff6e3cc2f69e87a958ae076b6c76e785a377
iuryroque <iury.roque@hotmail.com>
2021-08-02 15:11:18 -0300

    Adiciona soluçao proposta por a Trybe

--- modulo_1/bloco_4_intro_js/dia-3-javascript-logica-de-programacao-e-algoritmos/atividade1.js
+++ modulo_1/bloco_4_intro_js/dia-3-javascript-logica-de-programacao-e-algoritmos/atividade1.js
@@ -1,6 +1,32 @@
-let linha = 4;
-let coluna = 4;
+// Exercicio 1
+// Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+// n = 5
 
-for (let index = 0; index < linha; index++) {
-    console.log("1 ".repeat(coluna));
+// *****
+// *****
+// *****
+// *****
+// *****
+
+// Minha lógica
+
+let linha = 4;
+let coluna = linha;
+
+for (let index = 0; index < linha; index++){
+     console.log("*".repeat(coluna));
 }
+
+console.log(" ");
+// Lógica da Trybe
+
+let tamanhoDoQuadrado = 5;
+let symbol = '*';
+let inputLine = '';
+
+for (let lineIndex = 0; lineIndex < tamanhoDoQuadrado; lineIndex += 1) {
+  inputLine = inputLine + symbol;
+};
+for (let lineIndex = 0; lineIndex < tamanhoDoQuadrado; lineIndex += 1) {
+  console.log(inputLine);
+};
