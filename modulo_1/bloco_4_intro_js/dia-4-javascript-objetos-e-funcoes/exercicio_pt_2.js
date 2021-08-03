9c5ef6c0e3ddb0f4276bff7b69b699b4f09e9147
iuryroque <iury.roque@hotmail.com>
2021-08-02 21:58:15 -0300

    Finaliza exercicio 3 parte 2

--- modulo_1/bloco_4_intro_js/dia-4-javascript-objetos-e-funcoes/exercicio_pt_2.js
+++ modulo_1/bloco_4_intro_js/dia-4-javascript-objetos-e-funcoes/exercicio_pt_2.js
@@ -9,21 +9,36 @@ function invertePalavra(palavra) {
   }
 }
 
-console.log(invertePalavra("arara"));
-
+console.log("A palavra é " + invertePalavra("arara") + " que a palavra é um palindromo.");
+console.log("");
 //2ª Atividade
 
-function indiceMaior(array) {
+function indiceMaior(arrayMaior) {
   
   let maior = 0;
   let indiceNumero = 0;
-  for (let index = 0; index < array.length; index++) {
-    if (array[index] > maior) {
-      maior = array[index];
+  for (let index = 0; index < arrayMaior.length; index++) {
+    if (arrayMaior[index] > maior) {
+      maior = arrayMaior[index];
       indiceNumero = index;
     }
   }
   return indiceNumero;
 }
 
-console.log(indiceMaior([2, 3, 6, 7, 10, 1]));
+console.log("O menor indice é o: " + indiceMaior([2, 3, 6, 7, 10, 1]));
+console.log("");
+
+//3ª Atividade
+
+function indiceMenor(arrayMenor) {
+  let menor = 0;
+  for (let key in arrayMenor) {
+    if (arrayMenor[key] < menor){
+      menor = key;
+    }
+  }
+  return menor;  
+}
+
+console.log("O maior indice é o: " + indiceMenor([2, 4, 6, 7, 10, 0, -3]));
