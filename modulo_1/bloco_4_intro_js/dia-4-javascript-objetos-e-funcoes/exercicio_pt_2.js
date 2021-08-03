f5f36c01c12b44b1c318f4eaf906ba4f2ee9cfa5
iuryroque <iury.roque@hotmail.com>
2021-08-02 21:37:26 -0300

    Finaliza exercicio 1 parte 2

--- modulo_1/bloco_4_intro_js/dia-4-javascript-objetos-e-funcoes/exercicio_pt_2.js
+++ modulo_1/bloco_4_intro_js/dia-4-javascript-objetos-e-funcoes/exercicio_pt_2.js
@@ -1,3 +1,4 @@
+//1ª Atividade
 function invertePalavra(palavra) {
   let palavraInvertida = palavra.split("").reverse().join("")
 
@@ -8,4 +9,21 @@ function invertePalavra(palavra) {
   }
 }
 
-console.log(invertePalavra("arara")); 
+console.log(invertePalavra("arara"));
+
+//2ª Atividade
+
+function indiceMaior(array) {
+  
+  let maior = 0;
+  let indiceNumero = 0;
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] > maior) {
+      maior = array[index];
+      indiceNumero = index;
+    }
+  }
+  return indiceNumero;
+}
+
+console.log(indiceMaior([2, 3, 6, 7, 10, 1]));
