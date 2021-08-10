33d77677f6ff1b03c84515e6a327301438ddd475
iuryroque <iury.roque@hotmail.com>
2021-08-09 21:56:17 -0300

    Conclui questao 2

--- modulo_1/bloco_5_js_dom_eventos_web_storage/dia-1-javascript-dom-e-seletores/script.js
+++ modulo_1/bloco_5_js_dom_eventos_web_storage/dia-1-javascript-dom-e-seletores/script.js
@@ -1,2 +1,4 @@
-// atividade 1 
+// Questão 1 
 document.getElementById("previsao").innerText = "Empregado em TI";
+// atividade 2
+document.getElementsByClassName("main-content")[0].style.backgroundColor = "rgb(76,164,109)";
