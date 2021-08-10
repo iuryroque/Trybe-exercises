fc170e2c9ed7c7afd6b846b41cfd357d3996d0b3
iuryroque <iury.roque@hotmail.com>
2021-08-10 18:34:13 -0300

    Resolve exercicio 1

---
+++ modulo_1/bloco_5_js_dom_eventos_web_storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -0,0 +1,7 @@
+let h1 = document.createElement("h1");
+h1.innerHTML = "Exercício 5.2 - JavaScript DOM";
+document.body.appendChild(h1);
+
+let main = document.createElement("main");
+main.className = "main-content"
+document.body.appendChild(main)
