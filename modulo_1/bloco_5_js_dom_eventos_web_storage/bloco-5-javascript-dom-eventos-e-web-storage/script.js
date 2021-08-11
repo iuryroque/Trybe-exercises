2cf97bd8088cb025a4fd35a545464f3c6bc0aadc
iuryroque <iury.roque@hotmail.com>
2021-08-11 20:12:01 -0300

    Finaliza parte 2 da atividade

--- modulo_1/bloco_5_js_dom_eventos_web_storage/bloco-5-javascript-dom-eventos-e-web-storage/script.js
+++ modulo_1/bloco_5_js_dom_eventos_web_storage/bloco-5-javascript-dom-eventos-e-web-storage/script.js
@@ -10,14 +10,35 @@ function createDaysOfTheWeek() {
       weekDaysList.appendChild(dayListItem);
     };
   };
-  
+ 
   // Escreva seu código abaixo.
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
-let unordenedList = document.getElementById("days");
-let createLi = document.createElement("li");
-let addClassInLi = createLi.className = "day";
-function calenderDays() {
+  calenderDays()
 
+  function calenderDays() {
+    let classDay = document.querySelector('#days')
+    for (let index = 0; index < dezDaysList.length; index++) {
+      let day = dezDaysList[index]
+      let li = document.createElement('li')
+      li.innerText = day
+      classDay.appendChild(li)
+      li.className = 'day'
+  
+      if (day === 24 || day === 31 || day === 25) {
+        li.classList.add('holiday')
+      }
+      if (day === 4 || day === 11 || day === 18 || day === 25) {
+        li.classList.add('friday')
+      }
+    }
+  }
 
-}
+  function implementHolidayButton(feriados) {
+    let button = document.createElement("button");
+    let div = document.querySelector(".buttons-container");
+    div.appendChild(button);
+    button.innerText = "Feriados"
+    button.id = "btn-holiday";
+  }
+implementHolidayButton()
